d970397b393508dd5c5a59c511ce3d2d05c7cab4
DaltonC <daltocarter@outlook.com>
2023-01-13 22:43:39 -0600

    Add sprite type & changing the image to the surface parameter.

--- tile.py
+++ tile.py
@@ -4,9 +4,12 @@ from settings import *
 
 # CLass for empty positions in game map.
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, position, groups):
+    def __init__(self, position, groups, sprite_type, surface=pygame.Surface((TILE_SIZE, TILE_SIZE))):
         super().__init__(groups)
-        self.image = pygame.image.load("graphics/test/rock.png").convert_alpha()
+        # Type of sprite. i.e. enemy, obstacle...
+        self.sprite_type = sprite_type
+        # Image based on the surface parameter, default is a black surface.
+        self.image = surface
         # Rectangle of the tiles.
         self.rect = self.image.get_rect(topleft=position)
         # Set hit box to be slightly smaller than the rectangle.
